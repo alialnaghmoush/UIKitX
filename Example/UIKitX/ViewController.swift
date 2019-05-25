c16729585ebb101ffabd59aeaf8e9aecbd80039b
Ali AlNaghmousgh <alialnaghmoush@gmail.com>
2019-05-25 23:18:16 +0300

    Initial commit

---
+++ Example/UIKitX/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UIKitX
+//
+//  Created by alialnaghmoush on 05/25/2019.
+//  Copyright (c) 2019 alialnaghmoush. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
